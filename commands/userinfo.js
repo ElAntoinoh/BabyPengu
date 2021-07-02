74615c27f1b6d6880b3eda2e1f1b4f9400c68904
Antoine <toino1503@gmail.com>
2021-07-02 23:14:34 +0200

    Amélioration de la commande userinfo

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,9 +1,23 @@
+const { MessageEmbed } = require("discord.js");
+
 module.exports = {
     name: 'userinfo',
     description: 'Renvoie les informations de l\'utilisateur mentionné.',
 
     execute( client, message, args ) {
-        const user_mention = message.mentions.users.first();
-        message.channel.send(`Tag: ${user_mention.tag}`);
+        if( args.length === 1 )
+        {
+            const embed = new MessageEmbed()
+                .setColor("#dc143c")
+                .setThumbnail(message.mentions.users.first().avatarURL())
+                .addFields(
+                    { name: "Pseudo:", value: `${message.mentions.users.first().username}`},
+                    { name: "Tag:",    value: `${message.mentions.users.first().tag}`     }
+                );
+            
+            message.channel.send(embed);
+        }
+        else
+            message.channel.send("La commande §userinfo requiert un utilisateur en paramètre.");
     }
 }
