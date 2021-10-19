180166d622dfaab9466d9b03f6e4f6b852a7b04b
Antoine <toino1503@gmail.com>
2021-10-19 11:50:08 +0200

    Premiers ajouts liés à erela.js et lavalink

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,3 +1,5 @@
+const { Manager } = require("erela.js");
+
 module.exports = {
     name: 'ready',
     once: true,
@@ -8,5 +10,40 @@ module.exports = {
         let activities = ['§help', 'JavaScript', "tileman.io"];
 
         setInterval( () => client.user.setPresence({ activity: { name: `${activities[ Math.floor( Math.random()*activities.length ) ]}`, type: 'PLAYING' }, status: 'dnd' }), 5000 );
+
+        // Musique
+
+        client.manager = new Manager({
+            nodes: [ {
+                host:     client.config.LAVALINK_HOST,
+                port:     client.config.LAVALINK_PORT,
+                password: client.config.LAVALINK_PASSWORD,
+            } ],
+
+            send( id, payload ) {
+                const guild = client.guilds.cache.get(id);
+                if( guild ) guild.shard.send(payload);
+            },
+        })
+
+        client.manager.on("nodeConnect", node => console.log(`Node ${node.options.identifier} connected`));
+
+        client.manager.on("nodeError", (node, error) => console.log(`Node ${node.options.identifier} had an error: ${error.message}`));
+
+        client.manager.on("trackStart", (player, track) => {
+            client.channels.cache
+                .get(player.textChannel)
+                .send(`Now playing: ${track.title}`);
+        });
+
+        client.manager.on("queueEnd", (player) => {
+            client.channels.cache
+                .get(player.textChannel)
+                .send("Queue has ended.");
+        
+            player.destroy();
+        });
+
+        client.manager.init(client.user.id);
     },
 }
